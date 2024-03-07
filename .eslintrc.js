eb16cc3cce3a8d79dfdcb88915bdbaf42398b94d
masa <@>
2024-03-07 15:12:45 +0000

    Refactor.
    - Add comment in eslint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,36 +35,38 @@ module.exports = {
         'react'
     ],
     'rules': {
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-uses-react': 'off',
-        'react/jsx-uses-vars': 'error',
-        '@typescript-eslint/no-unused-vars': ['warn', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
-        '@typesctipt-eslint/explicit-module-boundary-types': 'off',
+        'react/react-in-jsx-scope': 'off', // Disable warning for missing React import in JSX
+        'react/jsx-uses-react': 'off', // Disable warning for unused React in JSX
+        'react/jsx-uses-vars': 'error', // Enable error for unused variables in JSX
+        '@typescript-eslint/no-unused-vars': ['warn', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }], // Enable warning for unused variables in TypeScript
+        '@typesctipt-eslint/explicit-module-boundary-types': 'off', // Disable warning for missing explicit return types in TypeScript
 
-        'no-else-return': ['error', {'allowElseIf': false }],
-        'no-eq-null': 'error',
-        'no-global-assign': ['error'],
-        'no-undefined': 'error',
-        'no-unused-vars': ['error'],
-        'no-use-before-define': ['error'],
-        'computed-property-spacing': ['error', 'never'],
-        'jsx-quotes': ['error'],
-        'no-console': ['warn'],
-        'quotes': ['error', 'single', {'avoidEscape': true}],
-        'comma-style': ['error', 'last'],
-        'indent': ['error', 4],
-        'semi': ['error'],
+        'no-else-return': ['error', {'allowElseIf': false }], // Require return statements in if-else blocks
+        'no-eq-null': 'error', // Disallow equality comparisons with null
+        'no-global-assign': ['error'], // Disallow global variable assignments
+        'no-undefined': 'error', // Disallow use of undefined variable
+        'no-unused-vars': ['error'], // Disallow unused variables
+        'no-use-before-define': ['error'], // Disallow use of variables before they are defined
+        'computed-property-spacing': ['error', 'never'], // Disallow spaces inside computed properties
+        'no-console': ['warn'], // Warn when using console.log
+
+        'jsx-quotes': ['error'], // Enforce double quotes in JSX attributes
+        'quotes': ['error', 'single', {'avoidEscape': true}], // Enforce single quotes
+        'comma-style': ['error', 'last'], // Enforce comma placement style
+        'indent': ['error', 4], // Enforce 4-space indentation
+        'semi': ['error'], // Require semicolons
         'no-multi-spaces': ['warn', {
             'exceptions':
             {
                 'Property': true,
                 'VariableDeclarator': true,
                 'ImportDeclaration': true
-            }}],
+            }}
+        ], // Warn for multiple consecutive spaces, except in certain cases
     },
     'settings': {
         'react': {
-            'version': 'detect',
+            'version': 'detect', // Detect React version
         },
     },
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './app';
 
 const ROOT_ID = 'root' as const;
 const rootElement = document.getElementById(ROOT_ID);
 
-if (!rootElement) throw new Error(`${ROOT_ID}の要素が見つかりません。`);
+if (!rootElement) throw new Error(`Not found ${ROOT_ID}.`);
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
