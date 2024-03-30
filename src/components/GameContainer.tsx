5d126622a26a993206e02dad352d173ba558c4cf
root <root@DESKTOP-5DISSKO>
2024-03-30 16:30:55 +0900

    Fixed the logic calculating the gameContainer size.

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
 import './GameContainer.css';
+import useWindowSize from './hooks/useWindowSize';
 
 interface GameContainerProps {
     children: React.ReactNode;
 }
 
 function GameContainer(props: GameContainerProps) {
+    const size = useWindowSize();
+    const height = size.height ? size.height * 0.8 : 0;
+    const width = size.width ? size.width * 0.8 : 0;
+
     return (
-        <div className="game-container board">
+        <div style={{ width: width, height: height }} className="game-container board">
             {props.children}
         </div>
     );
