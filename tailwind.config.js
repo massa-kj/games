1da8e33af052be09bf6386472b260d3c78733ecb
masam <massa-kj@users.noreply.github.com>
2025-10-19 08:29:24 +0900

    Add initial configuration files for ESLint, Prettier, Tailwind CSS, and TypeScript

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  ],
+  presets: [
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
