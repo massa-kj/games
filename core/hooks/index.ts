2a6c304f6966ddb73442d21c7c29a4f180670c28
masam <massa-kj@users.noreply.github.com>
2025-11-01 00:09:40 +0900

    feat(hooks): export additional audio hooks from useAdvancedSound

--- core/hooks/index.ts
+++ core/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useSettings } from './useSettings.js';
 export { useGameRegistry } from './useGameRegistry.js';
+export { useAdvancedSound, useGameAudio, useUIAudio } from './useAdvancedSound.js';
 export { useLocale } from './useLocale.js';
